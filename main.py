09ef32b653e24d10fcb32131289f0647ea0e7280
LeoGuoDNAS <lguo@wearetheone.com>
2023-08-10 14:13:56 -0400

    added CORS config

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.responses import RedirectResponse
 from chatbot import chatbot
 import json
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["https://rkguo.xyz"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins
+)
 
 @app.get("/")
 async def root():
